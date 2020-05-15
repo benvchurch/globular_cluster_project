7fe2134b92e708d48fbd08b56cb29a59848f6848
Ben Church <benvoulgarishchurch@gmail.com>
2020-05-15 03:01:56 -0400

    alowed stacking different numbers of images

--- stack.py
+++ stack.py
@@ -5,17 +5,23 @@ from astropy.utils.data import download_file
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
 
-
+import os.path
+from os import path
 
 def stack_images(image_file_base, outfile):
-	image_list = [ fits.open(image_file_base + str(n) + ".fits") if n > 9 else fits.open(image_file_base + '0' + str(n) + ".fits") \
-              for n in range(1,14) ]
+	num_to_stack = 1
+	while path.exists(image_file_base + str(num_to_stack) + ".fits") or path.exists(image_file_base + '0' + str(num_to_stack) + ".fits"):
+		num_to_stack += 1
+	
+	print 'stacking: ' + str(num_to_stack)
+	
+	image_list = [ fits.open(image_file_base + str(n) + ".fits") if n > 9 else fits.open(image_file_base + '0' + str(n) + ".fits") for n in range(1, num_to_stack) ]
 
 	print(image_list[0], image_list[0][0], image_list[0][0].header)
 
 	image_concat = [ image[0].data for image in image_list ]
 
-	final_image = np.sum(image_concat, axis=0)
+	final_image = np.mean(image_concat, axis=0)
 
 	# take background statistics of the image
 	mean, median, std = sigma_clipped_stats(final_image, sigma=10.0)
